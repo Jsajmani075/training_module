09ec2d3dd088ec894f891749417db360712ea891
Jsajmani075 <jagjotajmani075@gmail.com>
2025-02-10 20:42:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("Hello JI");
+let name = "shyamlal";
+let age = 25;
+console.log("Name:", name);
+console.log("Age:", age);
+
+function add(a, b) {
+ console.log(a + b);
+}
+
+module.exports={
+  add
+};
+
+ let number = 52;
+
+if (number > 0) {
+  console.log("The number is positive.");
+} else if (number < 0) {
+  console.log("The number is negative.");
+} else {
+  console.log("The number is zero.");
+}
+
